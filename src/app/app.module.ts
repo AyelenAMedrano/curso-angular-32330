06e098abbac9b9be3e81c29c4a198157b649207a
AyelenAMedrano <ayelen.medrano1999@gmail.com>
2022-11-30 00:21:14 -0300

    tarea-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ContenidoComponent } from './layout/contenido/contenido.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    ContenidoComponent,
   ],
   imports: [
     BrowserModule
