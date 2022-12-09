78c6114cfbb56e319e93214afd1f8b2a7f7654dd
AyelenAMedrano <ayelen.medrano1999@gmail.com>
2022-12-08 23:10:09 -0300

    tarea-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppComponent } from './app.component';
 import { TopbarComponent } from './layout/topbar/topbar.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { ContenidoComponent } from './layout/contenido/contenido.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
     SidebarComponent,
     ContenidoComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
